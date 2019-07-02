c64ddb72b06a25b749c2b2ecbd1472ba37b60033
Maruti Bandgar <47293485+marutibandgar@users.noreply.github.com>
2019-07-02 09:20:07 +0530

    Use Abstraction in Java

---
+++ Abstract Class.java
@@ -0,0 +1,13 @@
+# A class which is declared with the abstract keyword is known as an abstract class in Java.
+# Abstraction is a process of hiding the implementation details and showing only functionality to the user.
+# for example, sending SMS where you type the text and send the message. You don't know the internal processing about the message delivery.
+
+abstract class Bike{  
+  abstract void run();  
+}  
+class Honda4 extends Bike{  
+void run(){System.out.println("running safely");}  
+public static void main(String args[]){  
+ Bike obj = new Honda4();  
+ obj.run();  
+}  
