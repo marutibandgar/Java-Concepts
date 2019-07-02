06c0dc828b8fa8c07bcd7866a8f91b3a8d8854e9
Maruti Bandgar <47293485+marutibandgar@users.noreply.github.com>
2019-07-02 09:24:17 +0530

    Adding Some New Changes

--- Abstract Class.java
+++ Abstract Class.java
@@ -1,6 +1,9 @@
 # A class which is declared with the abstract keyword is known as an abstract class in Java.
 # Abstraction is a process of hiding the implementation details and showing only functionality to the user.
 # for example, sending SMS where you type the text and send the message. You don't know the internal processing about the message delivery.
+# An abstract class can have a data member, abstract method, method body (non-abstract method), constructor, and even main() method.
+
+
 
 abstract class Bike{  
   abstract void run();  
